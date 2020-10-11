bc0316785cca9d7e62ec1f9a225fdcad2fd8909d
Mohammad Montazeri <mohammad.montazeri.144@gmail.com>
2020-10-11 20:05:18 +0330

    Update IrPay.php

--- src/Gateways/IrPay.php
+++ src/Gateways/IrPay.php
@@ -123,23 +123,5 @@ class IrPay extends PaymentAbstract implements GatewayInterface
             'success' => false,
             'message' => 'خطایی رخ داده است.',
         ];
-
-    }
-
-    private function curl_post($url, $params)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-        ]);
-        $res = curl_exec($ch);
-        curl_close($ch);
-
-        return $res;
     }
 }
